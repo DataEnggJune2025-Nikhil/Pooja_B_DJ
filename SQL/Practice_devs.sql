bd3952ea76ddfcd7924414916189cfeb77078b77
poojab2813 <poojab1@umbc.edu>
2025-06-25 02:12:41 -0400

    Added practice questions of SQL first 20

---
+++ SQL/Practice_devs.sql
@@ -0,0 +1,98 @@
+USE devs_practice;
+-- Q.1 List the names and dates of birth of all programmers.
+
+SELECT programmer_name, DOB FROM programmers;
+
+-- Q.2 Display the names of programmers whose primary language is 'Python'.
+SELECT programmer_name FROM programmers
+WHERE Primary_Language = 'Python';
+
+-- Q.3 Show the names of programmers who joined the company after the year 2000.
+
+SELECT programmer_name 
+FROM programmers
+WHERE DOJ > '2000-12-31';
+
+-- Q.4 List the names and salaries of all female programmers.
+SELECT programmer_name
+FROM programmers
+WHERE GENDER = 'F';
+
+-- Q.5 Display the names of programmers sorted by their date of birth in ascending order.
+SELECT Programmer_Name, DOB
+FROM programmers
+ORDER BY DOB ASC;
+
+-- Q.6 Find the names of programmers whose secondary language is 'Java'.
+SELECT programmer_name 
+FROM programmers
+WHERE secondary_language = 'Java';
+
+-- Q.7 Show the details of programmers who earn a salary greater than $15,000.
+SELECT programmer_name
+FROM programmers 
+WHERE Salary > '15,000';
+
+-- Q.8 List the names of programmers whose primary language is 'C++' and who were born before 1970.
+
+SELECT programmer_name
+FROM programmers
+WHERE Primary_Language = 'C++'
+AND DOB < '1970-12-31';
+
+-- Q.9 Display the software names developed in 'Java'.
+SELECT Software_Name 
+FROM software
+WHERE Developed_In = 'Java';
+
+-- Q.10 List the courses studied at 'MIT'.
+SELECT Course
+FROM studies
+WHERE Institute = 'MIT';
+
+-- Q.11 What is the average salary of all programmers?
+SELECT AVG(Salary) as Average_salary
+FROM programmers;
+
+-- Q 12.How many programmers know 'Python' as their primary language?
+SElECT COUNT(*) as Python_programmers
+FROM programmers
+WHERE Primary_Language = 'Python';
+
+-- Q.13 What is the highest salary paid to a programmer?
+SELECT MAX(Salary) as Highest_Salary
+From programmers;
+
+-- Q.14 What is the total development cost of all software?
+SELECT SUM(DEvelopment_Cost) as Total_cost
+FROM software;
+
+-- Q.15 What is the minimum course fee paid by a programmer?
+SELECT MIN(Course_Fee) as Min_course_fee
+From studies;
+
+-- Q.16 How many software packages were sold in total?
+SELECT SUM(Sold) as Total_sold
+FROM software;
+
+-- Q.17 What is the average cost of a course at 'Columbia'?
+SELECT AVG(Course_fee) as Average_cost_columbia
+FROM studies
+WHERE Institute = 'Columbia';
+
+-- Q.18 How many male programmers are in the database?
+SELECT COUNT(*) as Male_programmers
+FROM programmers
+WHERE GENDER = 'M';
+
+-- Q.19 What is the sum of the software costs of packages developed in 'R'?
+SELECT SUM(Software_Cost) as cost_of_software
+FROM software
+WHERE Developed_In = 'R';
+
+-- Q.20 What is the maximum development cost for a package?
+SELECT MAX(Development_Cost) as Max_Cost 
+FROM software;
+
+
+ 
